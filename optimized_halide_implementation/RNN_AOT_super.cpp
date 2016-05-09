adecb1862949d2cf9ad077e926b94ac3c3055f8c
Bojian Han <bojianhan@CMU-922702.WV.CC.CMU.EDU>
2016-05-09 09:33:41 -0400

    Manage to remove the reduntant fprop_h fprop_y using list of pointers

---
+++ optimized_halide_implementation/RNN_AOT_super.cpp
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include "Halide.h"
+#include "CycleTimer.h"
+#include "halide_image_io.h"
+#include "init_h0.h"
+#include "fprop_h_t.h"
+#include "fprop_y_t.h"
+#include "loss.h"
+#include "init_dEdh_in_tp1.h"
+#include "bprop_dEdy_in.h"
+#include "bprop_dEdh_in.h"
+#include "bprop_Ghy.h"
+#include "bprop_Ghh.h"
+#include "bprop_Gxh.h"
+#include "grad_descent.h"
+
+using namespace Halide;
+
+#define T (10)
+#define ALL_DIMS (1000)
+#define NUM_INPUT (ALL_DIMS)
+#define NUM_HIDDEN (ALL_DIMS)
+#define NUM_OUTPUT (ALL_DIMS)
+#define BATCH_SIZE (ALL_DIMS)
+#define ROW_STRIDE (16)
+
+
+#define PARALLEL(func) \
+    func \
+        .tile(i, j, i_outer, j_outer, i_inner, j_inner, ALL_DIMS, ROW_STRIDE) \
+        .fuse(i_outer, j_outer, tile_index) \
+        .parallel(tile_index); \
+    func \
+        .tile(i_inner, j_inner, i_inner_outer, j_inner_outer, i_inner_inner, j_inner_inner, 16, 1) \
+        .vectorize(i_inner_inner)
+
+int main(int argc, char **argv) {
+    /* Name of Input Files */
+    char name[100];
+    /* Set the variables i,j,k */
+    Var i, i_outer, i_inner, i_inner_outer, i_inner_inner;
+    Var j, j_outer, j_inner, j_inner_outer, j_inner_inner;
+    Var k, tile_index;
+    /* Loading Image for Input X */
+    Func load_x; 
+    load_x(i,j,k) = 0.f;
+    for (int a = 1; a <= T; a++){
+        sprintf(name, "images/X_%d.png", a);
+        Image<uint8_t> img_x_t = Tools::load_image(name);
+        load_x(i, j, a) = cast<float>(img_x_t(i,j)) / 255.f - 0.5f;
+    }
+    Image<float> x = load_x.realize(NUM_INPUT, BATCH_SIZE, T + 1);
+
+    /* Loading Image for Target */
+    Func load_target;
+    load_target(i,j,k) = 0.f;
+    for (int a = 1; a <= T; a++){
+        sprintf(name, "images/T_%d.png", a);
+        Image<uint8_t> img_target_t = Tools::load_image(name);
+        load_target(i, j, a) = cast<float>(img_target_t(i,j)) / 255.f - 0.5f;
+    }
+    Image<float> target = load_target.realize(NUM_OUTPUT, BATCH_SIZE, T + 1);
+
+    /* Load h0 */
+    Func load_h;
+    load_h(i,j,k) = 0.f;
+    sprintf(name, "images/h0.png");
+    Image<uint8_t> img_h0 = Tools::load_image(name);
+    load_h(i,j,0) = cast<float>(img_h0(i,j)) / 255.f - 0.5f;
+    Image<float> h = load_h.realize(NUM_HIDDEN, BATCH_SIZE, T + 1);
+
+    /* Load y */
+    Image<float> y(NUM_OUTPUT, BATCH_SIZE, T+1);
+
+    /* Load Wxh */
+    Func load_Wxh;
+    sprintf(name, "images/Wxh.png");
+    Image<uint8_t> img_Wxh = Tools::load_image(name);
+    load_Wxh(i,j) = cast<float>(img_Wxh(i,j)) / 255.f - 0.5f;
+    Image<float> Wxh = load_Wxh.realize(NUM_HIDDEN,NUM_INPUT);
+
+    /* Load Whh */
+    Func load_Whh;
+    sprintf(name, "images/Whh.png");
+    Image<uint8_t> img_Whh = Tools::load_image(name);
+    load_Whh(i,j) = cast<float>(img_Whh(i,j)) / 255.f - 0.5f;
+    Image<float> Whh = load_Whh.realize(NUM_HIDDEN,NUM_HIDDEN);
+
+    /* Load Why */
+    Func load_Why;
+    sprintf(name, "images/Why.png");
+    Image<uint8_t> img_Why = Tools::load_image(name);
+    load_Why(i,j) = cast<float>(img_Why(i,j)) / 255.f - 0.5f;
+    Image<float> Why = load_Why.realize(NUM_OUTPUT,NUM_HIDDEN);
+
+    /* Training */
+    double start_time = CycleTimer::currentSeconds();
+    for (int epoch_num = 0; epoch_num < 10; epoch_num++) {
+
+        double epoch_time = CycleTimer::currentSeconds();
+        /* Forward propagation for all time */
+        //Func fprop_h, fprop_y; 
+        //fprop_h(i,j,k) = h(i,j,k); /* Initialize */
+        //fprop_y(i,j,k) = 0.f; /* Initialize */
+
+        /* Set up h_tm1 */
+        Image<float> h_tm1(NUM_HIDDEN,BATCH_SIZE);
+        init_h0(h.raw_buffer(), h_tm1.raw_buffer());
+
+        Image<float> h_ptr[T+1];
+        Image<float> y_ptr[T+1];
+        h_ptr[0] = h_tm1;
+        float total_loss = 0.f;
+        /* Forward propagation */
+        for (int t = 1; t <= T; t++){
+            /* Calculate h_t */
+            Image<float> h_t(NUM_HIDDEN,BATCH_SIZE);
+            fprop_h_t(t, x.raw_buffer(), h_tm1.raw_buffer(), 
+                Wxh.raw_buffer(), Whh.raw_buffer(), h_t.raw_buffer());
+            /* Calculate y_t */
+            Image<float> y_t(NUM_OUTPUT,BATCH_SIZE);
+            fprop_y_t(h_t.raw_buffer(), Why.raw_buffer(), y_t.raw_buffer());
+            /* Save for next iteration */
+            h_tm1 = h_t;
+            h_ptr[t] = h_t;
+            y_ptr[t] = y_t;
+            /* Save in all */
+            //fprop_h(i,j,t) = h_t(i,j);
+            //fprop_y(i,j,t) = y_t(i,j);
+            Image<float> avg_loss(1,1);
+            loss(t, y_t.raw_buffer(), target.raw_buffer(), avg_loss.raw_buffer());
+            total_loss += avg_loss(0,0);
+        }
+        //PARALLEL(fprop_h);
+        //fprop_h.realize(h);
+        //PARALLEL(fprop_y);
+        //fprop_y.realize(y);
+        //printf("Fprop Time: %f\n", CycleTimer::currentSeconds() - start_time);
+
+        /* Calculate Loss */
+        //Image<float> avg_loss(1,1,1);
+        //loss(y.raw_buffer(), target.raw_buffer(), avg_loss.raw_buffer());
+        printf("Average Loss: %f\n", total_loss / (T * BATCH_SIZE * NUM_INPUT));
+
+        /* Setup Gradients for Backprop */
+        Image<float> Gxh(NUM_HIDDEN, NUM_INPUT);
+        Image<float> Ghh(NUM_HIDDEN, NUM_HIDDEN);
+        Image<float> Ghy(NUM_OUTPUT, NUM_HIDDEN);
+        
+        /* Initialize tp1 dEdh */
+        Image<float> dEdh_in_tp1(NUM_HIDDEN, BATCH_SIZE);
+        init_dEdh_in_tp1(dEdh_in_tp1.raw_buffer());
+
+        /* Backward propagation */
+        for (int t = T; t > 0; t--){
+            Image<float> dEdy_in(NUM_OUTPUT, BATCH_SIZE);
+            bprop_dEdy_in(t, y_ptr[t].raw_buffer(), target.raw_buffer(), dEdy_in.raw_buffer());
+            Image<float> dEdh_in(NUM_OUTPUT, BATCH_SIZE);
+            bprop_dEdh_in(t, h_ptr[t].raw_buffer(), dEdh_in_tp1.raw_buffer(), dEdy_in.raw_buffer(), 
+                Whh.raw_buffer(), Why.raw_buffer(), dEdh_in.raw_buffer());
+            /* Preserve the variable for next iteration */
+            dEdh_in_tp1 = dEdh_in;
+            /* Add up the gradient */
+            bprop_Ghy(t, h_ptr[t].raw_buffer(), dEdy_in.raw_buffer(), Ghy.raw_buffer(), Ghy.raw_buffer());
+            bprop_Ghh(t, h_ptr[t-1].raw_buffer(), dEdh_in.raw_buffer(), Ghh.raw_buffer(), Ghh.raw_buffer());
+            bprop_Gxh(t, x.raw_buffer(), dEdh_in.raw_buffer(), Gxh.raw_buffer(), Gxh.raw_buffer());
+        }
+
+        /* Gradient Descent */
+        float learning_rate = 0.01f;
+        grad_descent(learning_rate, Wxh.raw_buffer(), Gxh.raw_buffer(),Wxh.raw_buffer());
+        grad_descent(learning_rate, Whh.raw_buffer(), Ghh.raw_buffer(),Whh.raw_buffer());
+        grad_descent(learning_rate, Why.raw_buffer(), Ghy.raw_buffer(),Why.raw_buffer());
+
+        printf("Epoch Time: %f\n", CycleTimer::currentSeconds() - epoch_time);
+    }
+    printf("Total: %f\n", CycleTimer::currentSeconds() - start_time);
+}
---
+++ optimized_halide_implementation/RNN_helper_super.cpp
@@ -0,0 +1,127 @@
+#include "Halide.h"
+#include <stdio.h>
+using namespace Halide;
+
+#define T (10)
+#define ALL_DIMS (1000)
+#define NUM_INPUT (ALL_DIMS)
+#define NUM_HIDDEN (ALL_DIMS)
+#define NUM_OUTPUT (ALL_DIMS)
+#define BATCH_SIZE (ALL_DIMS)
+#define ROW_STRIDE (16)
+
+#define PARALLEL(func) \
+    func \
+        .tile(i, j, i_outer, j_outer, i_inner, j_inner, ALL_DIMS, ROW_STRIDE) \
+        .fuse(i_outer, j_outer, tile_index) \
+        .parallel(tile_index); \
+    func \
+        .tile(i_inner, j_inner, i_inner_outer, j_inner_outer, i_inner_inner, j_inner_inner, 16, 1) \
+        .vectorize(i_inner_inner)
+
+#define PARALLEL_MATRIX(func) \
+    func \
+        .tile(i, j, i_outer, j_outer, i_inner, j_inner, 16, 64) \
+        .fuse(i_outer, j_outer, tile_index) \
+        .parallel(tile_index) \
+        .vectorize(i_inner)
+
+int main(){
+    /* Set the variables i,j,k */
+    Var i, i_outer, i_inner, i_inner_outer, i_inner_inner;
+    Var j, j_outer, j_inner, j_inner_outer, j_inner_inner;
+    Var k, tile_index;
+    /* Set the forward propagation iterator */
+    RDom rx(0, NUM_INPUT);
+    RDom rh(0, NUM_HIDDEN);
+    RDom ry(0, NUM_OUTPUT);
+    RDom rb(0, BATCH_SIZE);
+    RDom rl(0, NUM_OUTPUT, 0, BATCH_SIZE);
+    /* Set Image Params and Params */
+    Param<int32_t> t; /* Time Step */
+    Param<float> learning_rate; /* Time Step */
+    ImageParam x(type_of<float>(), 3);           /* 3D Input */
+    ImageParam h(type_of<float>(), 3);           /* 3D Hidden */
+    ImageParam h_t(type_of<float>(), 2);         /* Hidden Layer at Time t */
+    ImageParam h_tm1(type_of<float>(), 2);       /* Hidden Layer at Time t-1 */
+    ImageParam dEdh_in(type_of<float>(), 2);     /* Backprop error for dEdh before tanh at Time t */
+    ImageParam dEdh_in_tp1(type_of<float>(), 2); /* Backprop error for dEdh before tanh at Time t+1 */
+    ImageParam y(type_of<float>(), 3);           /* 3D Output */
+    ImageParam y_t(type_of<float>(), 2);         /* Output Layer at Time t */
+    ImageParam dEdy_in(type_of<float>(), 2);     /* Backprop error for dEdy before tanh at Time t */
+    ImageParam target(type_of<float>(), 3);      /* 3D Target */
+    ImageParam Wxh(type_of<float>(), 2);         /* Weight from Input to Hidden */
+    ImageParam Whh(type_of<float>(), 2);         /* Recurrent Weight of Hidden */
+    ImageParam Why(type_of<float>(), 2);         /* Weight from Hidden to Output */
+    ImageParam Gxh(type_of<float>(), 2);         /* Grad from Input to Hidden */
+    ImageParam Ghh(type_of<float>(), 2);         /* Recurrent Grad of Hidden */
+    ImageParam Ghy(type_of<float>(), 2);         /* Grad from Hidden to Output */
+    ImageParam weight(type_of<float>(), 2);       /* General Weight */
+    ImageParam grad(type_of<float>(), 2);         /* General Grad */
+
+    /* Setup h0 */
+    Func init_h0;
+    init_h0(i,j) = h(i,j,0);
+    PARALLEL(init_h0);
+    init_h0.compile_to_file("init_h0", {h});
+
+    /* Forward propagation for H at time t */
+    Func fprop_h_t;
+    fprop_h_t(i,j) = tanh(sum(x(rx,j,t) * Wxh(i,rx)) + sum(h_tm1(rh,j) * Whh(i,rh)));
+    PARALLEL_MATRIX(fprop_h_t);
+    fprop_h_t.compile_to_file("fprop_h_t", {t, x, h_tm1, Wxh, Whh});
+
+    /* Forward propagation for Y at time t */
+    Func fprop_y_t;
+    fprop_y_t(i,j) = tanh(sum(h_t(rh,j) * Why(i,rh)));
+    PARALLEL_MATRIX(fprop_y_t);
+    fprop_y_t.compile_to_file("fprop_y_t", {h_t, Why});
+
+    /* Calculate Loss */
+    Func loss;
+    loss(i,j) = sum(((y_t(rl.x,rl.y) - target(rl.x,rl.y,t)) * (y_t(rl.x,rl.y) - target(rl.x,rl.y,t))));
+    loss.compile_to_file("loss", {t, y_t, target});
+    
+    /* Setup dEdh_in_tp1 */
+    Func init_dEdh_in_tp1;
+    init_dEdh_in_tp1(i,j) = 0.f;
+    PARALLEL(init_dEdh_in_tp1);
+    init_dEdh_in_tp1.compile_to_file("init_dEdh_in_tp1",{});
+
+    /* Compute error with respect to (Why h_t) */
+    Func bprop_dEdy_in;
+    bprop_dEdy_in(i,j) = 2 * (y_t(i,j) - target(i,j,t)) * (1 - (y_t(i,j) * y_t(i,j)));
+    PARALLEL(bprop_dEdy_in);
+    bprop_dEdy_in.compile_to_file("bprop_dEdy_in",{t,y_t,target});
+
+    /* Compute error with respect to (Wxh x_t + Whh h_t) */
+    Func bprop_dEdh_in;
+    bprop_dEdh_in(i,j) = (sum(dEdy_in(ry,j) * Why(ry,i)) + sum(dEdh_in_tp1(rh,j) * Whh(rh,i)))*(1-h_t(i,j)*h_t(i,j));
+    PARALLEL_MATRIX(bprop_dEdh_in);
+    bprop_dEdh_in.compile_to_file("bprop_dEdh_in",{t, h_t, dEdh_in_tp1, dEdy_in, Whh, Why});
+
+    /* Calculate Gradients Ghy */
+    Func bprop_Ghy;
+    bprop_Ghy(i,j) = Ghy(i,j) + sum(h_t(j,rb) * dEdy_in(i,rb));
+    PARALLEL_MATRIX(bprop_Ghy);
+    bprop_Ghy.compile_to_file("bprop_Ghy",{t, h_t, dEdy_in, Ghy});
+
+    /* Calculate Gradients Ghh */
+    Func bprop_Ghh; 
+    bprop_Ghh(i,j) = Ghh(i,j) + sum(h_tm1(j,rb) * dEdh_in(i,rb));
+    PARALLEL_MATRIX(bprop_Ghh);
+    bprop_Ghh.compile_to_file("bprop_Ghh",{t,h_tm1,dEdh_in,Ghh});
+
+    /* Calculate Gradients Gxh */
+    Func bprop_Gxh;
+    bprop_Gxh(i,j) = Gxh(i,j) + sum(x(j,rb,t) * dEdh_in(i,rb));
+    PARALLEL_MATRIX(bprop_Gxh);
+    bprop_Gxh.compile_to_file("bprop_Gxh",{t,x,dEdh_in,Gxh});
+
+    /* Gradient Descent */
+    Func grad_descent;
+    grad_descent(i,j) = weight(i,j) - learning_rate * grad(i,j);
+    PARALLEL(grad_descent);
+    grad_descent.compile_to_file("grad_descent",{learning_rate,weight,grad});
+    return 0;
+}
